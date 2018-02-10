435d8117e5ce68927e7c7119ed344eb43cd4b307
Arpit Shah <arpitshah101@gmail.com>
2018-02-10 17:06:49 -0500

    Create empty board and print board function

---
+++ src/main/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	board := createEmptyBoard()
+	printBoard(board)
+
+}
+
+func createEmptyBoard() (board [10][10]string) {
+	// Empty board
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			board[i][j] = " "
+		}
+	}
+	return
+}
+
+func printBoard(board [10][10]string) {
+	fmt.Println("    1   2   3   4   5   6   7   8   9  10")
+	fmt.Println("  -----------------------------------------")
+
+	letters := "ABCDEFGHIJ"
+
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%c |", letters[i])
+		for j := 0; j < 10; j++ {
+			fmt.Printf(" %s |", board[i][j])
+		}
+		fmt.Println("\n  -----------------------------------------")
+	}
+}
