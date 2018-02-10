f1e762958b59bf040b33d2489430c6e874839739
Arpit Shah <arpitshah101@gmail.com>
2018-02-10 17:21:33 -0500

    Change board data to byte for efficient storage

--- src/main/main.go
+++ src/main/main.go
@@ -7,20 +7,19 @@ import (
 func main() {
 	board := createEmptyBoard()
 	printBoard(board)
-
 }
 
-func createEmptyBoard() (board [10][10]string) {
+func createEmptyBoard() (board [10][10]byte) {
 	// Empty board
 	for i := 0; i < 10; i++ {
 		for j := 0; j < 10; j++ {
-			board[i][j] = " "
+			board[i][j] = ' '
 		}
 	}
 	return
 }
 
-func printBoard(board [10][10]string) {
+func printBoard(board [10][10]byte) {
 	fmt.Println("    1   2   3   4   5   6   7   8   9  10")
 	fmt.Println("  -----------------------------------------")
 
@@ -29,7 +28,7 @@ func printBoard(board [10][10]string) {
 	for i := 0; i < 10; i++ {
 		fmt.Printf("%c |", letters[i])
 		for j := 0; j < 10; j++ {
-			fmt.Printf(" %s |", board[i][j])
+			fmt.Printf(" %c |", board[i][j])
 		}
 		fmt.Println("\n  -----------------------------------------")
 	}
