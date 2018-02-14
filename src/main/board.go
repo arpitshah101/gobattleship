c5f3bff1da8d7c8e1ca76dfb7d8986815d1ebfdf
Arpit Shah <arpitshah101@gmail.com>
2018-02-14 14:43:09 -0500

    Refactored code & created functions for generating ships
    - Moved board code to separate file and abstracted away internals
    - Created functions to add ships onto empty board at random locations

---
+++ src/main/board.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+var letters string
+
+type Board struct {
+	grid [10][10]byte
+}
+
+func (board *Board) AddShips() {
+	ships := [...]int{5, 4, 3, 3, 2}
+	for shipIndex := 0; shipIndex < len(ships); shipIndex++ {
+		shipSize := ships[shipIndex]
+		isValid := false
+		var row, col, direction int
+		for !isValid {
+			row, col, direction = generateRandomShipPositions()
+			// fmt.Printf("%c%d\n", letters[row], col)
+			isValid = board.CheckIfShipPositionIsValid(row, col, direction, shipSize)
+			if debug {
+				fmt.Printf("%d is valid\n", shipSize)
+			}
+		}
+		// valid position. ship can be set now
+		board.AddShip(row, col, direction, shipSize)
+		if debug {
+			fmt.Printf("Ship size %d added at %c%d!\n", shipSize, letters[row], col)
+		}
+	}
+}
+
+func (board *Board) SetValue(row, col int, value byte) bool {
+	if !board.IsEmptyLocation(row, col) {
+		return false
+	}
+
+	board.grid[row][col] = value
+	return true
+}
+
+func (board *Board) AddShip(row, col, direction, shipSize int) {
+	shipChar := strconv.Itoa(shipSize)[0]
+	switch direction {
+	case 0:
+		for startRow := row; startRow >= 0 && startRow > row-shipSize; startRow-- {
+			if !board.SetValue(startRow, col, shipChar) && debug {
+				fmt.Println("OVERWRITING VALUE")
+			}
+		}
+	case 1:
+		for startCol := col; startCol < 10 && startCol < col+shipSize; startCol++ {
+			if !board.SetValue(row, startCol, shipChar) && debug {
+				fmt.Println("OVERWRITING VALUE")
+			}
+		}
+	case 2:
+		for startRow := row; startRow < 10 && startRow < row+shipSize; startRow++ {
+			if !board.SetValue(startRow, col, shipChar) && debug {
+				fmt.Println("OVERWRITING VALUE")
+			}
+		}
+	case 3:
+		for startCol := col; startCol >= 0 && startCol > col-shipSize; startCol-- {
+			if !board.SetValue(row, startCol, shipChar) && debug {
+				fmt.Println("OVERWRITING VALUE")
+			}
+		}
+	}
+}
+
+func (board *Board) CreateEmptyBoard() {
+	letters = "ABCDEFGHIJ"
+	// Empty board
+	for i := 0; i < len(board.grid); i++ {
+		for j := 0; j < len(board.grid[i]); j++ {
+			board.grid[i][j] = ' '
+		}
+	}
+	return
+}
+
+func (board *Board) PrintBoard() {
+	fmt.Println("     1   2   3   4   5   6   7   8   9  10")
+	fmt.Println("   -----------------------------------------")
+
+	for i := 0; i < len(board.grid); i++ {
+		fmt.Printf(" %c |", letters[i])
+		for j := 0; j < len(board.grid[i]); j++ {
+			if debug {
+				fmt.Printf(" %c |", board.grid[i][j])
+			} else {
+				switch board.grid[i][j] {
+				case 'O':
+					fallthrough
+				case 'X':
+					fmt.Printf(" %c |", board.grid[i][j])
+				default:
+					fmt.Print("   |")
+				}
+			}
+
+		}
+		fmt.Println("\n   -----------------------------------------")
+	}
+}
+
+func (board *Board) GetValueAtLoc(row, col int) (value byte) {
+	return board.grid[row][col]
+}
+
+func (board *Board) IsEmptyLocation(row, col int) (isEmpty bool) {
+	return board.grid[row][col] == ' '
+}
+
+func generateRandomShipPositions() (row, col, direction int) {
+	row = random.Intn(10)
+	col = random.Intn(10)
+	direction = random.Intn(4)
+	return
+}
+
+func (board *Board) CheckIfShipPositionIsValid(row, col, direction, shipSize int) (isValid bool) {
+	// fmt.Printf("Cheching if ship location is valid at %c%d going in %d with size %d\n", letters[row], col, direction, shipSize)
+	switch direction {
+	case 0:
+		// up
+		if row-shipSize < 0 {
+			return false
+		}
+		// if there is already some ship in any of the proposed spaces, return false
+		for startRow := row; startRow >= 0 && startRow > row-shipSize; startRow-- {
+			if !board.IsEmptyLocation(startRow, col) {
+				return false
+			}
+		}
+
+	case 1:
+		// right
+		if col+shipSize > 10 {
+			return false
+		}
+
+		for startCol := col; startCol < 10 && startCol < col+shipSize; startCol++ {
+			if !board.IsEmptyLocation(row, startCol) {
+				return false
+			}
+		}
+
+	case 2:
+		// down
+		if row+shipSize > 10 {
+			return false
+		}
+
+		for startRow := row; startRow < 10 && startRow < row+shipSize; startRow++ {
+			if !board.IsEmptyLocation(startRow, col) {
+				return false
+			}
+		}
+
+	case 3:
+		// left
+		if col-shipSize < 0 {
+			return false
+		}
+
+		for startCol := col; startCol >= 0 && startCol > col-shipSize; startCol-- {
+			if !board.IsEmptyLocation(row, startCol) {
+				return false
+			}
+		}
+	}
+	return true
+}
--- src/main/main.go
+++ src/main/main.go
@@ -1,35 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"math/rand"
+	"time"
 )
 
+var random rand.Rand
+var debug bool
+
 func main() {
-	board := createEmptyBoard()
-	printBoard(board)
-}
-
-func createEmptyBoard() (board [10][10]byte) {
-	// Empty board
-	for i := 0; i < 10; i++ {
-		for j := 0; j < 10; j++ {
-			board[i][j] = ' '
-		}
-	}
-	return
-}
-
-func printBoard(board [10][10]byte) {
-	fmt.Println("    1   2   3   4   5   6   7   8   9  10")
-	fmt.Println("  -----------------------------------------")
-
-	letters := "ABCDEFGHIJ"
-
-	for i := 0; i < 10; i++ {
-		fmt.Printf("%c |", letters[i])
-		for j := 0; j < 10; j++ {
-			fmt.Printf(" %c |", board[i][j])
-		}
-		fmt.Println("\n  -----------------------------------------")
-	}
+	debug = true
+	s1 := rand.NewSource(time.Now().UnixNano())
+	random = *(rand.New(s1))
+	board := Board{}
+	board.CreateEmptyBoard()
+	board.PrintBoard()
+	board.AddShips()
+	board.PrintBoard()
 }
